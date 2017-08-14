2634c142eb57139c94fe1f5b48882c3c4dee9657
Ilan Ben Tal <ilandbt@gmail.com>
2017-08-14 11:14:41 +0300

    init project

---
+++ SimpleNetwork/SimpleNetwork/SimpleNetwork.h
@@ -0,0 +1,19 @@
+//
+//  SimpleNetwork.h
+//  SimpleNetwork
+//
+//  Created by Ilan Ben Tal on 14/08/2017.
+//  Copyright © 2017 Valera Health. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SimpleNetwork.
+FOUNDATION_EXPORT double SimpleNetworkVersionNumber;
+
+//! Project version string for SimpleNetwork.
+FOUNDATION_EXPORT const unsigned char SimpleNetworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SimpleNetwork/PublicHeader.h>
+
+
---
+++ SimpleNetwork/SimpleNetworkTests/SimpleNetworkTests.swift
@@ -0,0 +1,36 @@
+//
+//  SimpleNetworkTests.swift
+//  SimpleNetworkTests
+//
+//  Created by Ilan Ben Tal on 14/08/2017.
+//  Copyright © 2017 Valera Health. All rights reserved.
+//
+
+import XCTest
+@testable import SimpleNetwork
+
+class SimpleNetworkTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
